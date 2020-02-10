68d9c96d66004141ecf68fc5d7dbf81c0f86a44d
Jyothithomas <jyothithomas.05@gmail.com>
2020-02-10 17:39:51 -0500

    Initial Commit

---
+++ Day5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day5
+//
+//  Created by MacStudent on 2020-02-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
